e980b312c07a2cfc79405356c584f97ca7a529c4
Crowdbotics <team@crowdbotics.com>
2022-08-10 15:28:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "smartsocial_36707",
+  url: "https://smartsocial_36707.botics.co",
+  api: "https://smartsocial_36707.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
